1e34e5d19f38abd74213da816c9dd569e0b29157
Solomon Mensah <iam.itsmcodez@gmail.com>
2025-01-28 00:45:42 +0000

    feat: add batch export (closes #21)

---
+++ app/src/main/java/com/itsmcodez/assetstudio/models/ListItemModel.java
@@ -0,0 +1,17 @@
+package com.itsmcodez.assetstudio.models;
+
+public class ListItemModel {
+    private String name;
+
+    public ListItemModel(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
